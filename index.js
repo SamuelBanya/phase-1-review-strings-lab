9b23d68c435fcd8afb9b95109b0547280be55cae
Samuel Banya <sbanya@fastmail.com>
2022-04-25 21:23:39 -0400

    commit tree 6c62305b565645234f2d2096af8a7de42ee037ff

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Sam";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = welcomeMessage.slice(0, 7) + ', ' + currentUser.slice(0, 1) + '!';
